1babe6cfb113ab35d17d11b12aabab4195d5612e
Tonyfilho <amutizfilho@gmail.com>
2019-03-20 17:33:36 +0000

    aula 8 funções matematicas

---
+++ boson/Aula_08 Funções_matematicas2.py
@@ -0,0 +1,21 @@
+## temos que dar no IDLE o comando IMPORT MATH, para importar as funções matematicas, usaremos no TERMINAL os exemplos
+'''PODEMOS ATRIBUIR OS VALORES DE MATH DENTRO DE QUALQUER VARIAVEL'''
+
+# math.pi mostra o numero de PI
+# math.sqrt(x) Raiz quadrada
+# math.factorial(x) calcular numero fatorial
+# math.log10(x) calcula o logaritimo de X na base 10
+# math.pow (x,y) é o mesmo de ** ou X elevado a Y
+# math.ceil (x) Redonda o numero para maior
+# math.floor (x) Redonda o numero para menor
+
+'''EXISTEM MAIS FUNÇÕES MATEMATICAS DO QUE ESTAS, PODERÁ VER NAS DOCUMENTAÇÕES'''
+
+'''EXERCICIO, decubra o valor retornado da expressão abaixo'''
+''' math.log10(math.sqrt(math.pow(2,7)-2**2*7)), estamos passando função dentro de função .
+analizando: a Expressão é passada de dentro dos PARENTES 1º e depois para as funções externas, ficam nesta ordem:
+2**7 -2**2x2*7, este resultado SUBTRAIRÁ da função MATH.POW o o retorno cairá em MATH.SQRT que é a raiz quadrada e depois cairá MATH.LOG10  '''
+#resposta = 1
+
+
+

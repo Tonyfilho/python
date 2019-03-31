489b574d5a68ffd69f30562a9f16da701edaf8cc
Tonyfilho <oficinadosdrones@gmail.com>
2019-03-31 17:52:29 +0100

    exercio 12 e 13

--- curso_em_video/Exercicios_Python/desafio-12_aula07.py
+++ curso_em_video/Exercicios_Python/desafio-12_aula07.py
@@ -1,2 +1,9 @@
 '''faça um programa que LEIA o preço de uma produto, DÊ 5% de desconto
 e mostre  o seu novo  preço já com  o desconto '''
+
+produto = float(input('Digite o valor para produto: '))
+desconto = float(input('Digite o valor do Desconto: '))
+promocao = produto - (produto * desconto / 100)
+
+print(f'O Desconto é: {promocao}')
+
--- curso_em_video/Exercicios_Python/desafio-13_aula07.py
+++ curso_em_video/Exercicios_Python/desafio-13_aula07.py
@@ -1,2 +1,9 @@
 '''faça um algoritimo que leia o salario de  um funcionario e mostre seu
 novo salario com aumento de 15% '''
+
+salario = float(input('Digite o Seu Salário: '))
+aumento = float(input('Digite o Aumento de Sálario: '))
+salario_novo = salario + (salario * aumento / 100)
+
+print(f'O Salário novo é: {salario_novo}')
+

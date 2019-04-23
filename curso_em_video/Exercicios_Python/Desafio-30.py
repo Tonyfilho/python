418e010576af0136d18df340c5a1917796a1e250
Tonyfilho <oficinadosdrones@gmail.com>
2019-04-23 17:47:28 +0100

    desafio 30

--- curso_em_video/Exercicios_Python/Desafio-30.py
+++
@@ -1,2 +0,0 @@
-'''Exercício Python 030: Crie um programa que leia um número inteiro e mostre na tela se ele
-é PAR ou ÍMPAR.'''
---
+++ curso_em_video/Exercicios_Python/Desafio-30_numero_pares_e_impares.py
@@ -0,0 +1,17 @@
+'''Exercício Python 030: Crie um programa que leia um número inteiro e mostre na tela se ele
+é PAR ou ÍMPAR.'''
+
+'''O segredo está ali, no resto. O que sobra.
+Ele é o segredo de tudo.
+
+Para saber se um número é par ou ímpar, basta dividir ele por 2.
+Se for par, o resto é sempre 0, não sobra nada.
+Já se for ímpar, vai sempre ter resto 1.'''
+
+
+numero = int(input('Digite uma NUmero: '))
+if 0 == numero % 2:
+    print('Seu Numero {} é Par.'.format(numero))
+else:
+    print('seu numero {} é Impar'.format(numero))
+
